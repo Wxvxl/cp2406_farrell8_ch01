5229a6e094ad291db964e0ac0b249f37033ffff9
Calvin <metal123cool1115@gmail.com>
2022-11-16 08:13:09 +0800

    Add Practices

---
+++ Practices/FavoriteSong.java
@@ -0,0 +1,9 @@
+package Practices;
+
+public class FavoriteSong {
+        public static void main(String[] args)
+        {
+        System.out.println("I once had some sweet memories\n It's worth remains, all the same\n How can I remember those moments, sweetheart?\n Tell me TELL ME... If there's a man who can make anything \n Give me the tools to live in the past");
+        }
+    }
+    
